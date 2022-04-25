88b8c83922edc55f2a63ae3998dc6b87aca4d7b0
farukcokmez <hfcokmez@hotmail.com>
2022-04-25 15:49:34 +0300

    Service A sends basic request to Service B

---
+++ ServiceB.API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServiceB.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok(new { Id = id, Name = "Kalem", Price = 100, Stock = 200, Category = "Kalemler" });
+        }
+    }
+}
